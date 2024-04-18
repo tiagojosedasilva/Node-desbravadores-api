6c63109c6092a00edfc4d02b84190123303c004e
tiagojosedasilva <tiago.estudo.desenvolvimento@gmail.com>
2024-04-18 14:07:42 -0300

    ...

--- index.js
+++ index.js
@@ -1,46 +1,33 @@
-import { openDb } from './configDB.js';
 import express from 'express'
+import { createTable, deleteById, findAll, findByid, insert, update } from './service/postService.js';
 
 const app = express()
 app.use(express.json())
 
-openDb();
+createTable();
 
-let posts = []
-
-app.get("/", (request, response) => {
+app.get("/", async (request, response) => {
+    let posts = await findAll()
     return response.json(posts)
+})
+
+app.get("/:id", async (request, response) => {
+    let id = request.params.id;
+    let post = await findByid(id) 
+    return response.json(post)
 })
 
 app.post("/", (request, response) => {
-    const post = { 
-        id: posts.length + 1,
-        titulo : request.body.titulo, 
-        descricao : request.body.descricao, 
-        urlFoto : request.body.urlFoto, 
-        dataCriacao: new Date(),
-        dataAtualizacao: new Date()
-    }
-
-    posts.push(post)
-    return response.json(posts)
+    insert(request.body)
+    return response.json('status code: 200')
 })
 
-app.get("/:id", (request, response) => {
-    let id = request.params.id;
-    return response.json(posts[id])
-})
 
 app.delete("/:id", (request, response) => {
     let id = request.params.id;
-    let post = posts.indexOf(id)
-    if(id > -1){
-        posts.splice(id-1, 1)
-        for (let i = 0; i < posts.length; i++) {
-            const post = posts[i];
-            post.id = posts.indexOf(posts[i])+1
-            posts[i] = post
-        }
+
+    if(id >= 1){
+        deleteById(id);
         return response.json("removido com sucesso!")
     }else{
         return response.json("Algo inexperado aconteceu, por favor tente novamente!")
@@ -48,17 +35,12 @@ app.delete("/:id", (request, response) => {
 })
 
 app.patch("/:id", (request, response) => {
-
     let id = request.params.id;
-    let post = posts[id-1]
-
-    post.titulo = request.body.titulo, 
-    post.descricao = request.body.descricao, 
-    post.urlFoto = request.body.urlFoto, 
-    post.dataAtualizacao= new Date() 
-
-    posts[id-1] = post
-    return response.json(posts)
+    if (id > 0) {
+        let posts = update(id, request.body)
+        return response.json(posts)
+    }
+    return response.json('error')
 })
 
 app.listen(3000)
--- infraestrutura/config/configDB.js
+++ infraestrutura/config/configDB.js
@@ -3,7 +3,7 @@ import { open } from 'sqlite';
 
 export async function openDb(){
     return open({
-        filename: './database.db',
+        filename: './infraestrutura/database.db',
         driver: sqlite3.Database
     })
 }
